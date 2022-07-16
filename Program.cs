1f57983fcabd142d7f1e801c183fb1ec98b717ef
stas <79771234+ninahur1989@users.noreply.github.com>
2022-07-16 13:06:24 +0300

    starter

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module4_db
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
